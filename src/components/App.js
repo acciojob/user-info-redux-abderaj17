f753b300b27529475fb4d766d2494d18cff00c22
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-12-25 22:20:09 +0530

    skdjj

--- src/components/App.js
+++ src/components/App.js
@@ -26,11 +26,11 @@ const App = () => {
             onChange={handleEmailChange}
             />
           </label>
-           <div className="output">
+           <div className="output" name="Thomas Edward Stark">
           <h2>Preview</h2>
           <p><strong>Name:</strong>{name}</p>
           <p><strong>Email:</strong>{email}</p>
-          </div>
+        </div>
         </div>
     </div>
   );
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,23 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = {
-    name:'',
-    email:'',
+  name: "",
+  email: "",
 };
 
-const userSlice =  createSlice({
-    name: 'user',
-    initialState,
-    reducers:{
-        updateName:(state, action)=>{
-            state.name= action.payload;
-        },
-        updateEmail:(state, action)=>{
-            state.email = action.payload;
-        },
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName: (state, action) => {
+      state.name = action.payload;
     },
+    updateEmail: (state, action) => {
+      state.email = action.payload;
+    },
+  },
 });
 
-export const {updateName, updateEmail} = userSlice.actions;
+export const { updateName, updateEmail } = userSlice.actions;
 export default userSlice.reducer;
